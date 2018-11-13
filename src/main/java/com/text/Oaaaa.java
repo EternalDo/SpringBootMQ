baf4b16a08eb7e63e997b5767ac13eadf1996a98
duyongheng <1325558450@qq.com>
2018-11-13 23:18:37 +0800

    分支冲突提交

--- src/main/java/com/text/Oaaaa.java
+++ src/main/java/com/text/Oaaaa.java
@@ -8,13 +8,8 @@ public class Oaaaa {
  System.out.println("hahaha");
  System.out.println("laaaaaa");	
  System.out.println("dasdasdas");
-<<<<<<< HEAD
  System.out.println("1111111");
  System.out.println("aaaaa");
-=======
- System.out.println("1111111");
- System.out.println("22222222");	
->>>>>>> refs/heads/OneText
 	}
 
 }
