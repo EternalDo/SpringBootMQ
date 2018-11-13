cc68ee5193369a4f02b52ae35f51fb2a42fbb33a
duyongheng <1325558450@qq.com>
2018-11-13 23:20:18 +0800

    提交

--- src/main/java/com/text/Oaaaa.java
+++ src/main/java/com/text/Oaaaa.java
@@ -5,11 +5,7 @@ public class Oaaaa {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
- System.out.println("hahaha");
- System.out.println("laaaaaa");	
- System.out.println("dasdasdas");
- System.out.println("1111111");
- System.out.println("22222222");	
+ System.out.println("========");	
 	}
 
 }
